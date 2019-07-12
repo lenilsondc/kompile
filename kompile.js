d943ed06f124a29957550b604d2ed9ad1979fb63
Lenilson de Castro <lenilson.decastro@yahoo.com>
2019-07-12 00:27:13 -0300

    add engine code

---
+++ kompile.js
@@ -0,0 +1,27 @@
+function Kompile(html, model) {
+  var cur = 0;
+  var code = 'var o=\'\';';
+  var rgx = /(<%=?)(.*)?%>/gm;
+  var clean = function(i) {
+    return i.replace(/\r?\n/gm, '\\n');
+  };
+  var match;
+  while (match = rgx.exec(html)) {
+    var t = clean(html.substring(cur, match.index));
+    code += "o+='" + t + "';";
+    if (match[1] === '<%=')
+      code += "o+=" + match[2] + ";";
+    else
+      code += match[2] + ";";
+    cur = match.index + match[0].length;
+  }
+  code += "o+='" + clean(html.substring(cur)) + "';";
+  code += 'return o;';
+  return function(model) {
+    try {
+      return new Function(code).call(model);
+    } catch (e) {
+      return e.toString();
+    }
+  };
+}
